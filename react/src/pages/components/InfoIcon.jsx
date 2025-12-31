59ce1ac0954ad20a3c0e9444b63d8e6905f5d44c
kolin-ko <kkolinn4457@gmail.com>
2025-12-31 10:45:28 +0700

    simple tooltip

--- react/src/pages/components/InfoIcon.jsx
+++ react/src/pages/components/InfoIcon.jsx
@@ -1,23 +1,20 @@
-import { useTooltip } from "./TooltipContext"
+import { useState } from "react"
     
 function InfoIcon({ description, id }) {
-    const { openTooltip, setOpenTooltip } = useTooltip()
-    const isOpen = openTooltip === id
-
-    const handleClick = (e) => {
-        e.preventDefault()
-        setOpenTooltip(isOpen ? null : id)
-    }
+    const [showTooltip, setShowTooltip] = useState(false)
 
     return (
-        <span className="relative inline-block">
+        <span 
+            className="relative inline-block"
+            onMouseEnter={() => setShowTooltip(true)}
+            onMouseLeave={() => setShowTooltip(false)}
+        >
             <button
                 type="button"
                 className="inline-flex items-center justify-center w-5 h-5 text-xs font-bold bg-gray-600 rounded-full hover:bg-black cursor-help"
-                onClick={handleClick}
             > ?
             </button>
-            {isOpen && (
+            {showTooltip && (
                 <div className="absolute left-0 mt-2 w-48 p-2 text-sm text-blue-100 bg-gray-500 border border-black rounded shadow-lg z-10">
                     {description}
                 </div>
