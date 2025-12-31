a1eea0ba79dc95b372d0e550cbb297ef98068a1c
Christian Marcellino <christianmarcellino14@gmail.com>
2025-12-31 20:05:33 +0700

    Update

--- react/src/pages/predict-heart-attack.jsx
+++ react/src/pages/predict-heart-attack.jsx
@@ -11,7 +11,9 @@ function PredictHeartAttack() {
         <img src="https://media.tenor.com/I9qt03YKkjQAAAAe/monkey-thinking.png" alt="" />
     }
     return (
-        <div className="min-h-screen bg-base-200 flex items-center justify-center ">
+        <div className="min-h-screen bg-base-200 flex-col flex items-center justify-center ">
+            
+            <h1>Heart Attack Prediction</h1>
             <div className="bg-base-200 border rounded-2xl border-black flex items-center justify-center shadow-xl ">
                 <div className=" p-5 rounded-3xl max-w-6xl">
                     <FormPredict isloading={isloading} setIsloading={setIsloading} setResult={setResult}/>
